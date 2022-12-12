69d141458337396957ed0fea63264f1960d72ef6
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-12 12:21:30 +0530

    initial commit

---
+++ src/main/java/com/example/NestDigitalBoot_backend/NestDigitalBootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.NestDigitalBoot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestDigitalBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestDigitalBootBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NestDigitalBoot_backend/NestDigitalBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NestDigitalBoot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestDigitalBootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
