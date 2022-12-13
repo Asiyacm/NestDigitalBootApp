66f5ddd28d82a1578a64533d4c5d87821b7762a0
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-13 11:07:00 +0530

    leave commit

---
+++ src/main/java/com/example/NestDigitalBoot_backend/Model/Leave.java
@@ -0,0 +1,101 @@
+package com.example.NestDigitalBoot_backend.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "leaveTable")
+public class Leave {
+
+
+        @Id
+        @GeneratedValue
+        private int id;
+        private int empId;
+        private String type;
+        private String remarks;
+        private String fromDate;
+        private String toDate;
+        private String applyDate;
+        private int status;
+
+    public Leave() {
+    }
+
+    public Leave(int id, int empId, String type, String remarks, String fromDate, String toDate, String applyDate, int status) {
+        this.id = id;
+        this.empId = empId;
+        this.type = type;
+        this.remarks = remarks;
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+        this.applyDate = applyDate;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(String fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public String getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(String toDate) {
+        this.toDate = toDate;
+    }
+
+    public String getApplyDate() {
+        return applyDate;
+    }
+
+    public void setApplyDate(String applyDate) {
+        this.applyDate = applyDate;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
